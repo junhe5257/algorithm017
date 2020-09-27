7e55b4bfc6edaf681dc003cc19f475f148999f26
junhe5257 <wwyxhqc5257>
2020-09-27 23:14:00 +0800

    算法训练营第一周作业

---
+++ Week_01/src/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+/**
+ * 删除排序数组中的重复项
+ */
+public class RemoveDuplicates {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            if (nums.length == 0) return 0;
+            int i = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != nums[i]) {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i+1;
+        }
+    }
+}
